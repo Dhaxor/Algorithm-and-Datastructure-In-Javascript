fa043fe3cec3ff2e972ec8cb0e2615389c842395
Gain John <46064597+Dhaxor@users.noreply.github.com>
2020-06-12 20:25:05 +0100

    Update mergesort.js
    mergesort

--- mergesort.js
+++ mergesort.js
@@ -1,28 +1,24 @@
-function mergeme(left_part, right_part){
-var i= 0;
-var j = 0;
- 
-var results = [];
+ function mergeme(left_part,right_part) 
+{
+	var i = 0;
+	var j = 0;
+	var results = [];
 
-while (i < left_part.length || j < right_part.length) {
-    if (i === left_part.length) {
-        results.push(right_part[j]);
-        j++;
-        
-    }
-    else if (j === right_part.length || left_part[i] <= right_part[j]) {
-        results.push(left_part[i]);
-        i++;
-    }
-    else{
-        results.push(right_part[j]);
-        j++;
-    }
-
-    
+	while (i < left_part.length || j < right_part.length) {
+		if (i === left_part.length) {
+			// j is the only index left_part
+			results.push(right_part[j]);
+			j++;
+		} 
+      else if (j === right_part.length || left_part[i] <= right_part[j]) {
+			results.push(left_part[i]);
+			i++;
+		} else {
+			results.push(right_part[j]);
+			j++;
+		}
+	}
+	return results;
 }
-    return results;
 
-
-}
-console.log(mergeme([2,1,5], [4,0,2]));
+console.log(mergeme([1,3,4], [3,7,9]));
